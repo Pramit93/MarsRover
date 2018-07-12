f434ff92b800283556e7cb83adf0ad9aed8a747f
Pramit93 <pramitrn@gmail.com>
2018-07-12 14:18:25 +0530

    Checked

--- lib/rover.py
+++ lib/rover.py
@@ -1,8 +1,9 @@
+import re
+
 '''
 Rover class contains defination of each rover and contains method to display rover's current position and to run the rover.
 '''
 
-import re
 
 class Rover:
     def __init__(self, Id, x, y, head, p):
@@ -11,6 +12,7 @@ class Rover:
         self._y = y
         self._head = head
         self.p = p
+        self.flag=0 # a special variable which indicates whether the rover is impossible to operate due to illegal instruction sequence (0 - good, 1 - bad)
 
     def getPos(self):
         return str(self._x)+" "+str( self._y)
@@ -24,9 +26,11 @@ class Rover:
     def runRover(self, ins, heading):
         # Below code checks that the instruction sequence is not empty
         if (ins == ""):
+            self.flag = 1
             raise ValueError("Instruction sequence cannot be empty.")
-        #Below code checks that the instruction sequence is a string of only [LMR]
+        # Below code checks that the instruction sequence is a string of only [LMR]
         pattern = re.compile("^[MRL]*$")
+        # below code moves the rover to changes its direction based instructions
         shifts = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         cur_head = heading.index(self._head)
         if pattern.match(ins):
@@ -40,6 +44,7 @@ class Rover:
                 else:
                     x = self._x + shifts[cur_head][0]
                     y = self._y + shifts[cur_head][1]
+                    # Below code checks if the rover's path is blocked or whether it might cross the edge
                     if self.p.isOccupied(x, y):
                         raise ValueError("ERROR .... The position "+str((x,y))+" is already occupied so the rover got blocked.")
                     elif (x >= 0 and x <= self.p.x) and (y >= 0 and y <=self.p.y):
@@ -48,6 +53,7 @@ class Rover:
                     else:
                         raise ValueError("ERROR .... The Rover cannot be moved further "+str(heading[cur_head])+" or it will fall off the edge.")
         else:
+            self.flag = 1
             raise ValueError("Only values 'L', 'M' and 'R' are accepted!")
 
 
